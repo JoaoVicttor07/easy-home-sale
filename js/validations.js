44dac335be4fadb98c34baaeb1dff319584a5a59
João Victor <joaovictorsdc07@gmail.com>
2025-04-02 23:42:11 -0300

    feat(signup): add validation and success modal after registration
    Added validation for required fields and email format. Implemented a modal that appears after successful registration, guiding the user to the login page.

--- js/validations.js
+++ js/validations.js
@@ -1,13 +1,51 @@
 function registerUser(event) {
   event.preventDefault();
 
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  const confirmPassword = document.getElementById("confirm-password").value;
+  const email = document.getElementById("register-email").value;
+  const password = document.getElementById("register-password").value;
+  const confirmPassword = document.getElementById(
+    "register-confirm-password"
+  ).value;
+  const errorMessage = document.getElementById("register-error-message");
+  const sucessMessage = document.getElementById("register-success-message");
+  const successModal = document.getElementById("success-modal");
+  const goToLoginButton = document.getElementById("go-to-login");
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
+  // Validação: Todos os campos são obrigatórios
+  if (email === "" || password === "" || confirmPassword === "") {
+    errorMessage.textContent = "Todos os campos são obrigatórios";
+    errorMessage.style.display = "block";
+    return;
+  }
+
+  if (password === " ") {
+    errorMessage.textContent = "Senha é obrigatória";
+    errorMessage.style.display = "block";
+    return;
+  }
+
+  if (password.length < 8) {
+    errorMessage.textContent = "Sua senha deve ter no mínimo 8 caracteres";
+    errorMessage.style.display = "block";
+    return;
+  }
+
+  if (!emailRegex.test(email)) {
+    errorMessage.textContent = "E-mail inválido";
+    errorMessage.style.display = "block";
+    return;
+  }
+
+  if (confirmPassword === " ") {
+    errorMessage.textContent = "Confirmar senha é obrigatório";
+    errorMessage.style.display = "block";
+    return;
+  }
 
   if (password !== confirmPassword) {
-    alert("As senhas não coincidem!");
+    errorMessage.textContent = "As senhas devem ser iguais";
+    errorMessage.style.display = "block";
     return;
   }
 
@@ -15,15 +53,19 @@ function registerUser(event) {
   const userExists = users.some((user) => user.email === email);
 
   if (userExists) {
-    alert("E-mail já cadastrado!");
+    errorMessage.textContent = "E-mail já cadastrado!";
+    errorMessage.style.display = "block";
     return;
   }
 
   users.push({ email, password });
   localStorage.setItem("users", JSON.stringify(users));
 
-  alert("Cadastro realizado com sucesso!");
-  window.location.href = "index.html";
+
+  successModal.classList.remove("hidden");
+  goToLoginButton.addEventListener("click", () => {
+    window.location.href = "index.html";
+  });
 }
 
 function loginUser(event) {
